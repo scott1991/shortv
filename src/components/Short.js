d06a6d94f1d29521a4b9b480880925c52dfdd33b
scott1991 <cakepbm@gmail.com>
2023-11-28 03:19:25 +0800

    Draging show img

--- src/components/Short.js
+++ src/components/Short.js
@@ -1,23 +1,52 @@
 import './Short.css'
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 
 const Short = ({ title, cover, play_url }) => {
-  const imgRef = useRef(null);
+
+  const shortRef = useRef(null);
+  const [isPlaying, setIsPlaying] = useState(false);
 
   useEffect(() => {
-    if (imgRef.current) {
-      imgRef.current.ondragstart = () => false;
+    // if (shortRef.current) {
+    //   shortRef.current.ondragstart = () => false;
+    // } 
+
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          setIsPlaying(true); // 當 Short 可見時播放影片
+        } else {
+          setIsPlaying(false);
+        }
+      });
+    }, { threshold: 0.9 }); // 至少有50%的 Short 可見時觸發
+
+    if (shortRef.current) {
+      observer.observe(shortRef.current);
     }
-  }, []); // disable pic drag
+
+    return () => {
+      if (shortRef.current) {
+        observer.unobserve(shortRef.current);
+      }
+    };
+  }, []);
 
   return (
-    <div ref={imgRef}
-      className='short'
+    <div ref={shortRef} className='short'
+      
     >
-      {/* <img src={cover} alt={title} style={{ minHeight: '100svh', objectFit: 'cover' }} /> */}
-      <video playsInline webkit-playsinline="true" src={play_url} controls autoPlay muted loop style={{ minHeight: '100svh', objectFit: 'cover' }} 
-      type="application/x-mpegURL" />
+      {isPlaying ? (
+        <video
+          playsInline webkit-playsinline="true"
+          src={play_url} controls autoPlay muted loop
+          style={{ minHeight: '100svh', objectFit: 'cover' }}
+          type="application/x-mpegURL"
+        />
+      ) : (
+        <img src={cover} alt={title} style={{ minHeight: '100svh', objectFit: 'cover' }} />
+      )}
       <p className="short-title">{title}</p>
     </div>
   );
