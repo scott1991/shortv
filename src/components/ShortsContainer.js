63069c2383bfc5045dd5bb70b4f84c97a531ee40
scott1991 <cakepbm@gmail.com>
2023-11-28 13:16:01 +0800

    Fix scroll reset video time

--- src/components/ShortsContainer.js
+++ src/components/ShortsContainer.js
@@ -1,18 +1,21 @@
-import React, { useRef, useEffect } from 'react';
+import React, { useRef, useEffect, useState } from 'react';
 import Short from './Short.js';
 import './ShortsContainer.css';
 
 const ShortsContainer = ({ shorts, handleScroll, scrollPosition, handleProgress, playProgress }) => {
   const containerRef = useRef(null);
 
+  const [firstScroll, setFirstScroll] = useState(true);
+
   useEffect(() => {
     if (containerRef.current) {
       containerRef.current.scrollTop = scrollPosition;
+      setFirstScroll(false);
     }
   }, []);
 
   const onScroll = () => {
-    if (containerRef.current) {
+    if (containerRef.current && !firstScroll) {
       handleScroll(containerRef.current.scrollTop);
       handleProgress(0); // when scroll reset progress
     }
