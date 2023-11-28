bd6c168c110a21e140c6db345d837bf7f5aee5cc
scott1991 <cakepbm@gmail.com>
2023-11-28 14:04:55 +0800

    Use fetch instead of static data

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,48 +1,36 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import ShortsContainer from "./ShortsContainer.js";
+import setting from '../setting.js';
 import './Main.css';
 
+const { setting_host: host, setting_port: port } = setting;
 
 const Main = () => {
   const [activeList, setActiveList] = useState('following');
   const [scrollPositions, setScrollPositions] = useState({ following: 0, forYou: 0 });
   const [porgress, setPorgress] = useState({ following: 0, forYou: 0 })
 
-  const following = [
-    {
-      "title": "Audi_A4_S4",
-      "cover": "http://192.168.2.116:8088/images/Audi_A4_S4.png",
-      "play_url": "http://192.168.2.116:8088/media/Audi_A4_S4.m3u8"
-    },
-    {
-      "title": "Bugatti_Chiron",
-      "cover": "http://192.168.2.116:8088/images/Bugatti_Chiron.png",
-      "play_url": "http://192.168.2.116:8088/media/Bugatti_Chiron.m3u8"
-    },
-    {
-      "title": "Range_Rover_Sport_L322",
-      "cover": "http://192.168.2.116:8088/images/Range_Rover_Sport_L322.png",
-      "play_url": "http://192.168.2.116:8088/media/Range_Rover_Sport_L322.m3u8"
-    }
-  ];
+  const [lists, setLists] = useState({ following: [], forYou: [] });
 
-  const forYou = [
-    {
-      "title": "Rolls_Royce_Ghost",
-      "cover": "http://192.168.2.116:8088/images/Rolls_Royce_Ghost.png",
-      "play_url": "http://192.168.2.116:8088/media/Rolls_Royce_Ghost.m3u8"
-    },
-    {
-      "title": "Toyota_Camry_XV70",
-      "cover": "http://192.168.2.116:8088/images/Toyota_Camry_XV70.png",
-      "play_url": "http://192.168.2.116:8088/media/Toyota_Camry_XV70.m3u8"
-    },
-    {
-      "title": "Volkswagen_Golf_7",
-      "cover": "http://192.168.2.116:8088/images/Volkswagen_Golf_7.png",
-      "play_url": "http://192.168.2.116:8088/media/Volkswagen_Golf_7.m3u8"
-    }
-  ];
+  useEffect(() => {
+    fetch(`http://${host}:${port}/following_list`)
+      .then(response => response.json())
+      .then(data => setLists(prevLists => ({
+        ...prevLists,
+        following: data.items
+      })))
+      .catch(error => console.error(error))
+
+      fetch(`http://${host}:${port}/for_you_list`)
+      .then(response => response.json())
+      .then(data => setLists(prevLists => ({
+        ...prevLists,
+        forYou: data.items
+      })))
+      .catch(error => console.error(error))
+  }, []);
+
+
 
   const handleScrollImpl = (listName, scrollPosition) => {
     setScrollPositions((prevScrollPositions) => ({
@@ -61,25 +49,27 @@ const Main = () => {
   return (
     <div className="main">
       <div className="button-container">
-        <span className="tab-button" onClick={() => setActiveList('following')}>Following</span>
-        <span className="tab-button" onClick={() => setActiveList('forYou')}>For You</span>
+        <span className={`tab-button ${activeList === 'following' ? 'active' : ''}`}
+          onClick={() => setActiveList('following')}>Following</span>
+        <span className={`tab-button ${activeList === 'forYou' ? 'active' : ''}`}
+          onClick={() => setActiveList('forYou')}>For You</span>
       </div>
 
-      {activeList === 'following' &&
+      {activeList === 'following' && lists.following.length > 0 &&
         <ShortsContainer
           handleScroll={(scrollPosition) => handleScrollImpl('following', scrollPosition)}
           scrollPosition={scrollPositions.following}
           handleProgress={(currentTime) => handleProgressImpl('following', currentTime)}
           playProgress={porgress.following}
-          shorts={following}
+          shorts={lists.following}
         />}
-      {activeList === 'forYou' &&
+      {activeList === 'forYou' && lists.forYou.length > 0 &&
         <ShortsContainer
           handleScroll={(scrollPosition) => handleScrollImpl('forYou', scrollPosition)}
           scrollPosition={scrollPositions.forYou}
           handleProgress={(currentTime) => handleProgressImpl('forYou', currentTime)}
           playProgress={porgress.forYou}
-          shorts={forYou}
+          shorts={lists.forYou}
 
         />}
     </div>
---
+++ src/setting.js
@@ -0,0 +1,4 @@
+export default {
+  setting_host: '192.168.2.116',
+  setting_port: 8088
+}
