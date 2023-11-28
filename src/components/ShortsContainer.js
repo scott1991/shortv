eb6cbceb220ec2e9c1f66ff472cdb9c3489152ba
scott1991 <cakepbm@gmail.com>
2023-11-28 14:05:25 +0800

    Add commets and modify css

--- src/components/ShortsContainer.js
+++ src/components/ShortsContainer.js
@@ -5,7 +5,7 @@ import './ShortsContainer.css';
 const ShortsContainer = ({ shorts, handleScroll, scrollPosition, handleProgress, playProgress }) => {
   const containerRef = useRef(null);
 
-  const [firstScroll, setFirstScroll] = useState(true);
+  const [firstScroll, setFirstScroll] = useState(true); // prevent first scroll reset time
 
   useEffect(() => {
     if (containerRef.current) {
